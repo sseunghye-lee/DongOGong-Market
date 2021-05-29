8b9a251ef1d707199f0650c85537f174294252cc
sgy101303 <59862669+sgy101303@users.noreply.github.com>
2021-05-29 15:13:40 +0900

    가연 - 상품 등록 페이지 유저 세션 담기

--- src/main/java/com/dongogong/controller/ProductController.java
+++ src/main/java/com/dongogong/controller/ProductController.java
@@ -3,12 +3,22 @@ package com.dongogong.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/apply/product.do")
 public class ProductController {
     @GetMapping
-    public String applyProduct() {
-        return "applyProduct";
+    public String applyProduct(HttpServletRequest request, HttpServletResponse response) {
+        UserSession userSession =
+                (UserSession) WebUtils.getSessionAttribute(request, "userSession");
+
+        ModelAndView mav = new ModelAndView("applyProduct");
+        mav.addObject("userSession", userSession);
+        return "mav";
     }
 }
